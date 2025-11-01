27aefa355cdd2758cf37731f2734d3c82a38257d
corentin48982344 <corentin.coleno.contact@gmail.com>
2025-11-01 23:55:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -936,7 +936,7 @@ const [userProfile, setUserProfile] = useState({ firstName: '', lastName: '' });
     return (
     <div style={{
         backgroundColor: theme.bg,
-        minHeight: '100vh',
+        Height: '100vh',
         position: 'fixed',
         display: 'flex',
         alignItems: 'center',
