a9369f48291f914a9bf59c4d284ab7fb03b7e04e
corentin48982344 <corentin.coleno.contact@gmail.com>
2025-11-01 23:31:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -993,7 +993,7 @@ const [userProfile, setUserProfile] = useState({ firstName: '', lastName: '' });
               value={formData.firstName || ''}
               onChange={(e) => setFormData({...formData, firstName: e.target.value})}
               style={{
-                width: '50%',
+                width: '90%',
                 padding: '15px',
                 marginBottom: '20px',
                 borderRadius: '12px',
@@ -1032,7 +1032,7 @@ const [userProfile, setUserProfile] = useState({ firstName: '', lastName: '' });
                 }
               }}
               style={{
-                width: '100%',
+                width: '90%',
                 padding: '15px',
                 marginBottom: '30px',
                 borderRadius: '12px',
